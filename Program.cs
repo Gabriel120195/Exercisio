cfde4cafcce0654091c283674c7ca53ce2b74bda
Gabriel120195 <103151153+Gabriel120195@users.noreply.github.com>
2022-10-01 20:37:43 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercisio03_03_J
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = int.Parse(Console.ReadLine());
+            int conta = 1;
+
+            for (int i = 1; i < numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    conta = conta + 1;
+                }
+            }
+
+            if (conta == 2)
+                Console.WriteLine("O Número {0} é Primo!", numero);
+            else
+                Console.WriteLine("O Número {0} Não é Primo", numero);
+        }
+    }
+}
+
